f2991113f806b35c60191e865c08780f90c6d2fd
lingyenlee <lingyenlee@gmail.com>
2019-05-10 09:20:40 +0200

    ps0 and 1 done

---
+++ ps0/ps0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 12:54:40 2019
+
+@author: apple
+"""
+import numpy
+x = int(input("Enter number x:"))
+y = int(input("Enter number y:"))
+print(x**y)
+print(numpy.log2(x))
